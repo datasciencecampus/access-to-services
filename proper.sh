82710b78146ba18e0b6ebb4cf727f2391dfdd29d
Phil Stubbings <phil@parasec.net>
2019-08-08 14:32:29 +0100

    proper.sh skel

---
+++ proper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# cli propeR interface
+# =============================================================================
+# exposes the following propeR functions
+#   pointToPoint
+#   pointToPointLoop
+#   pointToPointNearest
+#   pointToPointTime
+# 
+#   isochrone
+#   isochroneTime
+#   isochroneMulti
+#
+
+pointToPoint() {
+
+}
+
+pointToPointLoop {
+
+}
+
+pointToPointNearest {
+
+}
+
+pointToPointTime {
+
+}
+
+isochrone {
+
+}
+
+isochroneTime {
+
+}
+
+isochroneMulti {
+
+}
+
+
+
+
