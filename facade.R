0499c68e2a1fe2f41ed4edb403dfb5c794d7bc79
phil8192 <phil@parasec.net>
2019-08-12 16:25:39 +0000

    facade for accepting args

--- facade.R
+++ facade.R
@@ -1,4 +1,4 @@
-library(propeR)
+suppressMessages(library(propeR))
 
 connect <- (function() {
   otp <- NULL
@@ -17,12 +17,9 @@ generate <- function(fun, src_file, dst_file, ...) {
   do.call(fun, list(otpcon=otp, originPoints=src.points, destinationPoints=dst.points, ...))
 }
 
-generate_point_to_point <- function(src_file, dst_file, ...) {
-  generate(pointToPoint, src_file, dst_file, ...)
+if(!interactive()) {
+  args.str <- commandArgs(trailingOnly=T)
+  args.lst <- eval(parse(text=paste0("list(", args.str, ")")))
+  stopifnot("fun" %in% names(args.lst))
+  do.call(generate, args.lst)
 }
-
-generate_isochrone <- function(src_file, dst_file, ...) {
-  generate(isochrone, src_file, dst_file, ...)
-}
-
-#generate_point_to_point(src_file="propeR/inst/extdata/origin.csv", dst_file="propeR/inst/extdata/destination.csv", output.dir="/tmp/a2s", startDateAndTime="2019-08-02 12:00:00", mapOutput=T)
