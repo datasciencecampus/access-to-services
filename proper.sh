03f6cccc8c99344bece1d8b843562a9b20ec6006
phil8192 <phil@parasec.net>
2019-08-12 16:24:25 +0000

    sync

--- proper.sh
+++ proper.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
 # cli propeR interface
 # =============================================================================
-# exposes the following propeR functions
-#   pointToPoint
-#   pointToPointLoop
-#   pointToPointNearest
-#   pointToPointTime
-# 
-#   isochrone
-#   isochroneTime
-#   isochroneMulti
+# propeR functions
+#   * pointToPoint
+#   * pointToPointLoop
+#   * pointToPointNearest
+#   * pointToPointTime
 #
+#   * isochrone
+#   * isochroneTime
+#   * isochroneMulti
+# =============================================================================
+# example:
+#
+# ./proper.sh 'fun=pointToPoint,src_file="propeR/inst/extdata/origin.csv", dst_file="propeR/inst/extdata/destination.csv", output.dir="/tmp/a2s", startDateAndTime="2019-08-02 12:00:00", mapOutput=T'
 
-pointToPoint() {
+# --x=123 --y=321 -> x=123,y=321
+# -x=abc --y=heh  -> x=abc,y=heh
+args="$(echo $@ |sed 's/ -\+/,/g; s/^-\+//')"
 
-}
+if [[ "$args" != *"fun="* ]] ;then
+  echo "missing fun"
+  exit 1
+fi
 
-pointToPointLoop {
+echo "$args"
 
-}
-
-pointToPointNearest {
-
-}
-
-pointToPointTime {
-
-}
-
-isochrone {
-
-}
-
-isochroneTime {
-
-}
-
-isochroneMulti {
-
-}
-
-
-
-
+# invoke facade with parsed args
+Rscript --vanilla facade.R "$args"
